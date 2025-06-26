8710147aa3021fee328d290317dfbe48dcd5180a
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-26 13:18:02 +0200

    date structs

---
+++ Sources/Structures/date-structures.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum DateDistanceUnit: String, CaseIterable {
+    case days
+    case months
+    case years
+    case hours
+    case minutes
+    case seconds
+    case milliseconds
+    case combined
+}
