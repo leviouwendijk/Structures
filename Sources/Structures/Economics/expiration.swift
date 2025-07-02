96d7468435ea966e30c02df6fd6cf5c543dab959
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-02 12:32:26 +0200

    breaking change in DateDistanceUnit dependencies (adding case: week)

---
+++ Sources/Structures/Economics/expiration.swift
@@ -0,0 +1,41 @@
+import Foundation
+import plate
+
+public struct ExpirationDateRange: Sendable {
+    public let start: Date
+    public let end: Date
+    
+    public init(
+        start: Date,
+        end: Date
+    ) {
+        self.start = start
+        self.end = end
+    }
+
+    // public var string: String {
+
+    // }
+}
+
+public struct ExpirationSetting: Sendable {
+    public let dates: ExpirationDateRange
+
+    public init(using dates: ExpirationDateRange) {
+        self.dates = dates
+    }
+    
+    public init(from start: String, to end: String) throws {
+        let s = try start.date()
+        let e = try end.date()
+
+        self.dates = ExpirationDateRange(start: s, end: e)
+    }
+
+    public init(from start: String, interval: DateComponents) throws {
+        let s = try start.date()
+        let e = s + interval
+
+        self.dates = ExpirationDateRange(start: s, end: e)
+    }
+}
