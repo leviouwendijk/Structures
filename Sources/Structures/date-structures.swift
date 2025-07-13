9596fa2358ac15f2e7d6aaf773f33a30e13f8525
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-13 18:49:01 +0200

    std dateformatter psql

--- Sources/Structures/date-structures.swift
+++ Sources/Structures/date-structures.swift
@@ -51,3 +51,20 @@ public enum DateDistanceUnit: String, CaseIterable, Sendable {
     }
 }
 
+public struct StandardDateFormatter {
+    public static var postgres: ISO8601DateFormatter {
+        let fmt = ISO8601DateFormatter()
+        fmt.formatOptions = [
+            .withInternetDateTime,
+            .withFractionalSeconds,
+            .withColonSeparatorInTimeZone
+        ]
+        return fmt
+    }
+}
+
+public extension Date {
+    var postgresTimestamp: String {
+        StandardDateFormatter.postgres.string(from: self)
+    }
+}
