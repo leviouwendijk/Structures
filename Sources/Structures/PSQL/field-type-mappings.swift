8640ce97e84b3dfce4dec8ada47f706c46376518
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-13 23:57:57 +0200

    PSQLFieldTypeRegistry

---
+++ Sources/Structures/PSQL/field-type-mappings.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public enum PSQLFieldTypeRegistryError: Error, LocalizedError {
+    case noMapForSpecifiedTable(String)
+
+    public var errorDescription: String? {
+        switch self {
+            case .noMapForSpecifiedTable(let tableQuery):
+                return "No mapping was found in registry for provided table string query: \(tableQuery)"
+        }
+    }
+}
+
+public struct PSQLFieldTypeRegistry {
+    private static let mapping: [String: [String: PSQLType]] = [
+        "captcha_tokens": [
+            "id":            .uuid,
+            "ip_address":    .text,
+            "hashed_token":  .text,
+            "expires_at":    .timestamptz,
+            "usage_count":   .integer,
+            "max_usages":    .integer,
+            "invalidated":   .boolean,
+            "created_at":    .timestamptz
+        ],
+    ]
+
+    public static func mapping(table: String) throws -> [String: PSQLType] {
+        if let table = mapping[table] {
+            return table
+        } else {
+            throw PSQLFieldTypeRegistryError.noMapForSpecifiedTable(table)
+        }
+    }
+}
