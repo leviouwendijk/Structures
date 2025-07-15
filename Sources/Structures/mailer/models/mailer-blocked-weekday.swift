542ed74f4c4b6f19340555cb19242e730d544378
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-07-15 13:20:33 +0200

    ensure nilencoded

--- Sources/Structures/mailer/models/mailer-blocked-weekday.swift
+++ Sources/Structures/mailer/models/mailer-blocked-weekday.swift
@@ -11,4 +11,18 @@ public struct MailerAPIBlockedWeekday: Encodable {
         self.weekday = weekday
         self.limit = limit
     }
+
+    enum CodingKeys: String, CodingKey {
+        case weekday, limit
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var c = encoder.container(keyedBy: CodingKeys.self)
+        try c.encode(weekday, forKey: .weekday)
+        if let l = limit {
+            try c.encode(l, forKey: .limit)
+        } else {
+            try c.encodeNil(forKey: .limit)
+        }
+    }
 }
