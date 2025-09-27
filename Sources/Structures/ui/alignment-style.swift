60671771052ca91265efbdc8cc3ef8f6599055ce
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-27 18:48:22 +0200

    adding ui/

---
+++ Sources/Structures/ui/alignment-style.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public enum AlignmentStyle {
+    case leading
+    case trailing
+    case center
+}
---
+++ Sources/Structures/ui/build-information-display-comps.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public enum BuildInformationDisplayComponents {
+    case version
+    case name
+    case author
+    case description
+    // case versionPrefix
+    case latestVersion
+}
