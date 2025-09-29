82cc01444b9212d8fa515535e6b4e395a06fc5ab
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-09-29 14:36:35 +0200

    make store a class

--- Sources/Structures/text-messages/reusable-text-message/reusable-text-message-store.swift
+++ Sources/Structures/text-messages/reusable-text-message/reusable-text-message-store.swift
@@ -1,7 +1,28 @@
 import Foundation
 
-public struct ReusableTextMessageStore: Codable, Sendable {
-    public var messages: [ReusableTextMessageObject]
+// public struct ReusableTextMessageStore: Codable, Sendable {
+//     public var messages: [ReusableTextMessageObject]
+    
+//     public init(
+//         messages: [ReusableTextMessageObject] = []
+//     ) {
+//         self.messages = messages
+//     }
+
+//     public mutating func add(message: ReusableTextMessageObject) -> Void {
+//         messages.append(message)
+//     }
+
+//     public mutating func add(messages: [ReusableTextMessageObject]) -> Void {
+//         for m in messages {
+//             add(message: m)
+//         }
+//     }
+// }
+
+@MainActor
+public class ReusableTextMessageStore: ObservableObject {
+    @Published public var messages: [ReusableTextMessageObject]
     
     public init(
         messages: [ReusableTextMessageObject] = []
@@ -9,11 +30,11 @@ public struct ReusableTextMessageStore: Codable, Sendable {
         self.messages = messages
     }
 
-    public mutating func add(message: ReusableTextMessageObject) -> Void {
+    public func add(message: ReusableTextMessageObject) -> Void {
         messages.append(message)
     }
 
-    public mutating func add(messages: [ReusableTextMessageObject]) -> Void {
+    public func add(messages: [ReusableTextMessageObject]) -> Void {
         for m in messages {
             add(message: m)
         }
